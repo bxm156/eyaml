a8086927e0c315a725741519d24f761ac3700417
Bryan Marty <bxm156@case.edu>
2014-09-06 12:36:46 -0700

    Intial files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='eyaml',
+    version='0.1',
+    description='Thin Wrapper around PyYAML with env support',
+    author='Bryan Marty',
+    author_email='bryan@bryanmarty.com',
+    license='MIT',
+    keywords="yaml env environment",
+    url='http://www.bryanmarty.com/',
+    packages=['eyaml'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
